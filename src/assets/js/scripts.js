ada476d08c0ebea9062fa3876b3f8555b9fef457
harrisch <charlie.harris@wwt.com>
2016-09-17 15:26:29 -0500

    Adds normal styling for coming soon page

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -6,14 +6,26 @@
     doc.sidebarVisible = false;
 
     var body = $('body');
-    var sectionTopOffset = $('#top').offset().top;
+    var topSection = $('#about');
 
-    var topSection = $('#top-section');
-    // $(window).resize();
+    var sectionTopOffset = topSection.offset().top;
+
+    $(window).resize();
     // function resizeBackground() {
     //     topSection.height($(window).height() - 60);
     // }
     // resizeBackground();
+
+    var firstWaypoint = new Waypoint({
+      element: document.getElementById('about'),
+      handler: function(direction) {
+        if (direction == 'down') {
+          body.removeClass('no-header');
+        } else if (direction == 'up') {
+          body.addClass('no-header');
+        }
+      }
+    });
 
     $('#menu-reveal').click(function() {
         doc.sidebarVisible = !doc.sidebarVisible;
@@ -22,11 +34,25 @@
         } else {
           body.removeClass('menuOpen');
         }
-    })
+    });
 
     $('.section-link').click(function() {
       body.removeClass('menuOpen');
       doc.sidebarVisible = false;
-    })
+    });
+
+    function isScrollEnough() {
+      if (window.pageYOffset >= sectionTopOffset) {
+        return true
+      } else {
+        return false
+      }
+    }
+
+    if (isScrollEnough()) {
+      body.removeClass('no-header');
+    } else {
+      body.addClass('no-header');
+    }
   })
 })(jQuery);
